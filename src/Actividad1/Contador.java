fcc5c4e003b4c39ccb7603adfe10dbf042a9bf61
Jorge Rui Da Silva Barrios <jorgedasilva.n98@gmail.com>
2018-05-24 16:41:50 +0200

    Arreglado cambios en tarea 1

--- src/Actividad1/Contador.java
+++ src/Actividad1/Contador.java
@@ -3,25 +3,35 @@ package Actividad1;
 import java.util.concurrent.Semaphore;
 
 public class Contador {
-	
-	
+
+
 	static final  int NITER=1000000;
 	static volatile int count = 0;
-	
-		public static void main(String[] args) throws InterruptedException {
-			// T000 Auto-generated method stub
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
 			Semaphore s = new Semaphore(1);
-			System.out.print("contador");
+			
 			Tarea hl= new Tarea("h1",s);
 			Tarea h2= new Tarea("h2",s);
+			
 			hl.start();
 			h2.start();
 			hl.join();
 			h2.join();
+			
 			if (count < 2 * NITER)
-				System.out.print("\n"+" BOOM! count es " + count+" deberia ser "+2*NITER);
-				else
-					System.out.print("\n"+" OK! count es "+ count);
+				System.out.println("BOOM! count es " + count + " deberia ser " + (2*NITER));
+			else
+				System.out.println("OK! count es " + count);
+			
+		} catch (InterruptedException error) 
+		{
+			System.err.println("Error en el main principal");
+			error.printStackTrace();
+		}
 	}
 
 }
--- src/Actividad1/Tarea.java
+++ src/Actividad1/Tarea.java
@@ -2,31 +2,38 @@ package Actividad1;
 
 import java.util.concurrent.Semaphore;
 
-public class Tarea extends Thread {
-	String c;
-	Semaphore s;
-	public Tarea(String arg, Semaphore s1){
-		c = arg;
-		s = s1;
+public class Tarea extends Thread 
+{
+	
+	private String nombre;
+	private Semaphore semáforo;
+	
+	public Tarea(String nombre, Semaphore semáforo)
+	{
+		this.nombre = nombre;
+		this.semáforo = semáforo;
 	}
 	
 	
 	public void run()
 	{
-		int tmp;
-		try {
-			s.acquire();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		try 
+		{
+			int tmp;
+			semáforo.acquire();
+			for (int i =0; i< Contador.NITER;i++){
+				tmp = Contador.count; /* copy the global count locally */
+				tmp = tmp+1; /* increment the local copy */
+				Contador.count = tmp;
+				
+			}
+			
+			semáforo.release();
+		} catch (InterruptedException e) 
+		{
+			System.err.println("Error en: " + getClass().getName());
 			e.printStackTrace();
 		}
 		
-		for (int i =0; i< Contador.NITER;i++){
-			tmp = Contador.count; /* copy the global count locally */
-			tmp = tmp+1; /* increment the local copy */
-			Contador.count = tmp;
-			
-		}
-		s.release();
 	}
 }
